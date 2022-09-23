1a101a5b50c184a1a4fba529106fa6a7574bba33
marcelo benites <marcelobenitespro85@gmail.com>
2022-09-23 20:51:34 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <title>Agência de Criação de Sites | Rio Grande do Sul</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="agência de marketing digital, agência de criação de sites,agência de sites,automação de marketing">
+    <meta name="author" content="Agência Code By Benites" >
+</head>
+<body>
+    <header id="home">
+        <div class="container">
+            <div class="menu-topo">
+                <div class="logo"></div>
+                    <nav class="desktop">
+                        <ul>
+                            <li><a href="#sobre">Sobre</a></li>
+                            <li><a href="#servicos">Serviços</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            <li class="btn-nav" ><a href="#contato">Quero um orçamento</a></li>
+                        </ul>
+                    </nav><!--desktop-->
+                    <nav class="mobile">
+                        <h3><i class="fa fa-bars"></i></h3>
+                        <ul>
+                            <li><a href="#sobre">Sobre</a></li>
+                            <li><a href="#servicos">Serviços</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            <li class="btn-nav" ><a href="#contato">Quero um orçamento</a></li>
+                        </ul>
+                    </nav><!--mobile-->
+                    <div class="clear"></div>
+             </div><!--menu-topo--->
+             <section class="chamada-header">
+                    <h2>Você vendendo mais <br /> com automação web.</h2>
+                    <p>Nós criamos soluções de marketing para sua empresa vender mais, da melhor forma.</p>
+                     <form>
+                         <!--<input type="email" name="" placeholder="Seu email...." required>-->
+                         <a href="https://bit.ly/3BG8eFl"><button class="btn-whats"  type="submit"><i class="fa fa-whatsapp" aria-hidden="true"></i> <p>Quero Saber Mais</p></button></a>
+                     </form>
+                </section><!---chamada-header--->
+        </div><!--container-->
+        <a href="#home"><div class="btn-top" style="background-image: url('imagens/seta.png');"></div></a>
+        <a href="https://bit.ly/3BG8eFl"><div class="button-watts" style="background-image: url('imagens/whatsapp.png');"></div></a>
+    </header>
+    <section id="sobre" class="descricao-agencia">
+      <div class="container">
+      <p>Com anos de experiência em desenvolvimento web e automação de marketing, a <b>Agência Code By Benites</b> traz soluções que ajudam o seu time a vender mais tanto em segmentos B2C quanto B2B.</p>
+      </div><!--container-->
+    </section><!---descricao-->
+    <section class="descricao-agencia2">
+       <div class="container">
+          <div class="texto-descricao">
+          <h3>Nós somos uma agência digital. Trabalhamos seu posicionamento, conteúdo e estética, sempre focado em conversão.</h3>
+				<p>Converse com a nossa equipe ainda hoje e crie um plano de marketing para o seu negócio!</p>
+          </div><!--texto-descricao--->
+          <img src="imagens/mockup1.jpg" alt="trabalho agência code by benites" title="trabalho agência code by benites">
+          <div class="clear"></div>
+       </div><!---container-->
+    </section><!---descricao-agencia2-->
+    <section class="descricao-agencia3">
+         <div class="container">
+            <div class="box-depoimento">
+            <p>“Temos a  Agência Code By Benites como parceiro estratégico nos nossos planejamento em plataforma digital. Sempre nos trazendo soluções inovadoras e adequadas às nossas necessidades e dos nossos clientes. Super recomendo!“</p>
+				<br/>
+				<p><b>Glaúcio - Voodoo Branded Content</b></p>
+            </div><!---box-depoimento-->
+         </div><!--container--->
+    </section><!---agencia3---->
+    <section class="servicos">
+       <div class="container">
+          <div id="servicos" class="title-servicos">
+              <h3>Como podemos ajudar a sua empresa<br /><i class="fa fa-angle-down" ></i></h3>
+          </div><!--servicos--->
+          <div class="flex-servicos">
+          <div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img3.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>E-mail Marketing</h2>
+							<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img1.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Páginas de Venda</h2>
+							<p>Criamos páginas de venda 100% otimizadas e voltadas para conversões. Solicite um orçamento e tenha hoje mesmo a sua página para vender na internet.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img2.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Páginas de Captura e Inscrição</h2>
+							<p>Capture o e-mail dos visitantes em seu site e crie uma base de leads para sua empresa.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->	
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img4.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>SEO & Marketing de Conteúdo</h2>
+							<p>Aplicamos SEO no seu website para o mesmo ficar bem posicionado nos mecanismos de busca e ser achado facilmente.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img5.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>ADWords & Facebook ADS</h2>
+							<p>Criamos campanhas altamente otimizadas para o seu negócio gerar mais vendas pelo menor custo.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="imagens/img6.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Consultoria de Marketing Digital</h2>
+							<p>Precisa de ajuda e planejamento de marketing no seu negócio? Fale com a Agência Code By Benites, hoje mesmo, para
+							criarmos um plano de marketing a longo prazo e 100% voltado para vendas.</p>
+							<a href="#contato">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->					
+
+			
+          </div><!--flex-servicos-->
+       </div><!---container---->
+    </section><!---servicos-->
+
+    <section class="portfolio">
+       <div class="container">
+            <h2>Algumas empresas que tivemos a oportunidade de atuar</h2>
+            <div class="empresas">
+                 <div class="empresa-single">
+                      <img src="imagens/bianchini.png" alt="">
+                 </div><!---empresas-single--->
+                 <div class="empresa-single">
+                      <img src="imagens/rancho.png" alt="">
+                 </div><!---empresas-single--->
+                 <div class="empresa-single">
+                      <img src="imagens/hoepcke.png" alt="">
+                 </div><!---empresas-single--->
+                 <div class="empresa-single">
+                      <img src="imagens/monguilhott.png" alt="">
+                 </div><!---empresas-single--->
+            </div><!--empresas--->
+       </div><!---container---->
+    </section><!--portfolio--->
+
+    <section id="contato" class="formulario" >
+         <div class="container">
+            <h2>Um orçamento ou alguma dúvida</h2>
+            <form>
+            <a href="https://bit.ly/3BG8eFl"><button class="btn-whats"  type="submit"><i class="fa fa-whatsapp" aria-hidden="true"></i> <p   style="top:-2px;  position: relative;" >Quero Saber Mais</p></button></a>
+            </form>
+         </div><!--container--->
+    </section><!--formulario--->
+    <footer>
+        <div class="container">
+            <div class="box-footer">
+                <h2>Endereço</h2>
+                <p>Rua Sapiranga, 111 - Sapucaia do Sul/RS, 93226-476, Brasil</p>
+            </div><!---box-footer--->
+            <div class="box-footer" style="text-align:center;">
+                <h2>Receba as nossas news</h2>
+                <form>
+                <a href="https://bit.ly/3BG8eFl"><button   class="btn-whats" type="submit"><i class="fa fa-whatsapp"  style=" font-size: 23px;" aria-hidden="true"></i> <p  style="top:-2px;  position: relative;" >Quero Saber Mais</p></button></a>                
+               </form>
+            </div><!---box-footer--->
+            <div class="clear"></div>
+        </div><!---container---->
+    </footer>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/menuScroll.js"></script>
+    <script>
+        $('nav.mobile h3').click(function(){
+			$('nav.mobile').find('ul').slideToggle();
+		})
+    </script>
+</body>
+</html>
---
+++ js/menuScroll.js
@@ -0,0 +1,26 @@
+$(function(){
+
+   
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop': offSetTop});
+
+        return false;
+
+    })
+
+
+    $('.btn-top').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop': offSetTop});
+
+        return false;
+
+    })
+
+});
+
